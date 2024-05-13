265e70023ef624e5971dadb57776bddd947604e9
AyoubNajjout <ayoubnajout1@gmail.com>
2024-05-13 07:56:40 +0100

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   const [imgSrc, setImgSrc] = useState(null);
 
   const nameRef = useRef();
+  const fileRef = useRef();
+
 
   const getImg = () => {
     const username = nameRef.current.value;
@@ -20,8 +22,24 @@ function App() {
     });
   }
 
+  const postImg = () => {
+    const file = fileRef.current.files[0];
+    const name = "user1"
+    const formData = new FormData(); 
+    formData.append('file', file);
+    axios.post(`http://127.0.0.1:8000/api/v1/auth/${name}/add-picture`, formData)
+    .then((res) => {
+      console.log("uploaded",formData);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }
+
   return (
     <>
+    <input type="file" ref={fileRef}/>
+      <button onClick={postImg}>post img</button>
       <input type="text" ref={nameRef}/>
       <button onClick={getImg}>Get Image</button>
       {imgSrc && <img src={imgSrc} alt="" />} {/* Render the image if imgSrc is available */}
